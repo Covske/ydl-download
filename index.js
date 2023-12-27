01f2d4a187e33eeac9c3e881930bf1aa25072572
Erycovsk <98654999+Erycovski@users.noreply.github.com>
2023-12-27 07:59:59 -0300

    agora essa bomba funcona shorts

--- index.js
+++ index.js
@@ -1,70 +1,88 @@
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const readline = require('readline');
-const sanitize = require('sanitize-filename'); // sanitize faz com que os caracteres do 'titutloAarquivo' sejam todos válidos
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const readline = require("readline");
+const sanitize = require("sanitize-filename"); // sanitize faz com que os caracteres do 'titutloAarquivo' sejam todos válidos
 
 // Cria uma interface de leitura de linha para interagir com o usuário no console
 const leitor = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
+
+function isYouTubeShorts(link) {
+    // Verifica se a URL contém a parte específica de shorts do YouTube
+    return link.includes('/shorts/');
+}
+
+
+function converterLinkShortsParaWatch(linkShorts) {
+    // Expressão regular para encontrar "shorts/" e substituir por "watch?v="
+    const regex = /\/shorts\//;
+  
+    // Substituir "shorts/" por "watch?v="
+    const linkWatch = linkShorts.replace(regex, "/watch?v=");
+  
+    return linkWatch;
+  }
 
 // Pergunta ao usuário o link do vídeo
 leitor.question("link do vídeo: ", function (res) {
-    // Obtém informações básicas sobre o vídeo usando o módulo ytdl-core
-    ytdl.getBasicInfo(res).then((resInfo) => {
+  // Obtém informações básicas sobre o vídeo usando o módulo ytdl-core
+  ytdl.getBasicInfo(res).then((resInfo) => {
+    // Sanitiza o título do vídeo removendo caracteres inválidos para nomes de arquivo
+    const tituloArquivo = sanitize(resInfo.videoDetails.title);
 
-        // Sanitiza o título do vídeo removendo caracteres inválidos para nomes de arquivo
-        const tituloArquivo = sanitize(resInfo.videoDetails.title);
+    // URL do vídeo
+    var videoURL = res;
 
-        // URL do vídeo
-        const videoURL = res;
+    if (isYouTubeShorts(videoURL)) {
+       videoURL =  converterLinkShortsParaWatch(videoURL)
+    } else {
+        return null
+    }
+    
 
-        // Diretório de saída para salvar o arquivo
-        const outputDirectory = 'downloads';
+    // Diretório de saída para salvar o arquivo
+    const outputDirectory = "downloads";
 
-        // Caminho completo do arquivo de saída
-        const outputFilePath = `${outputDirectory}/${tituloArquivo}.mp4`;
+    // Caminho completo do arquivo de saída
+    const outputFilePath = `${outputDirectory}/${tituloArquivo}.mp4`;
 
-        // Verifica se o diretório de saída existe e o cria se não existir
-        if (!fs.existsSync(outputDirectory)) {
-            fs.mkdirSync(outputDirectory);
-        }
+    // Verifica se o diretório de saída existe e o cria se não existir
+    if (!fs.existsSync(outputDirectory)) {
+      fs.mkdirSync(outputDirectory);
+    }
 
-        // Opções para a qualidade do vídeo a ser baixado
-        const options = {
-            quality: 'highestvideo',
-            filter: 'videoandaudio'
-        };
+    // Opções para a qualidade do vídeo a ser baixado
+    const options = {
+      quality: "highestvideo",
+      filter: "videoandaudio",
+    };
 
-        // Cria um stream de download usando ytdl-core
-        const stream = ytdl(videoURL, options);
+    // Cria um stream de download usando ytdl-core
+    const stream = ytdl(videoURL, options);
 
-        // Evento de progresso, exibindo a porcentagem de conclusão do download
-        stream.on('progress', (downloaded, total) => {
-            const percent = (downloaded / total) * 100;
-            console.log(`Baixando: ${percent.toFixed(2)}%`);
-        });
-
-        // Evento de conclusão do download
-        stream.on('end', () => {
-            console.log('Download concluído!');
-            // Fecha a interface de leitura de linha após o download ser concluído
-            leitor.close();
-        });
-
-        // Evento de erro durante o download
-        stream.on('error', (error) => {
-            console.error('Ocorreu um erro:', error);
-            // Fecha a interface de leitura de linha em caso de erro
-            leitor.close();
-        });
-
-        // Pipe (encaminha) o stream de download para um arquivo no sistema de arquivos
-        stream.pipe(fs.createWriteStream(outputFilePath));
+    // Evento de progresso, exibindo a porcentagem de conclusão do download
+    stream.on("progress", (downloaded, total) => {
+      const percent = (downloaded / total) * 100;
+      console.log(`Baixando: ${percent.toFixed(2)}%`);
     });
+
+    // Evento de conclusão do download
+    stream.on("end", () => {
+      console.log("Download concluído!");
+      // Fecha a interface de leitura de linha após o download ser concluído
+      leitor.close();
+    });
+
+    // Evento de erro durante o download
+    stream.on("error", (error) => {
+      console.error("Ocorreu um erro:", error);
+      // Fecha a interface de leitura de linha em caso de erro
+      leitor.close();
+    });
+
+    // Pipe (encaminha) o stream de download para um arquivo no sistema de arquivos
+    stream.pipe(fs.createWriteStream(outputFilePath));
+  });
 });
-
-
-
-
